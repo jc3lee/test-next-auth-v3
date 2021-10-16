2059776310db6055eeed24adadf6a533627aabf5
jclee <dev.if.ljc@gmail.com>
2021-10-16 16:03:07 +0200

    has access & refresh token

--- pages/api/secret/index.js
+++ pages/api/secret/index.js
@@ -1,8 +1,11 @@
 import { getSession } from "next-auth/client";
+import { getToken } from "next-auth/jwt";
 
 export default async (req, res) => {
-  const session = await getSession({ req });
-
+  const session = await getSession({ req, });
+  console.log("session", session);
+  // const token = await getToken({ req })
+  // console.log("token", token);
   if (session) {
     res.send({
       content: "Welcome to the secret page",
